97aa2b893d2091d521a303cb9791c45654c835b3
krishna.sapare <krishna.sapare@target.com>
2017-12-08 13:43:42 +0530

    Removed spaces

--- plugin_test.go
+++ plugin_test.go
@@ -37,7 +37,7 @@ func TestPlugin(t *testing.T) {
 
 			err := plugin.Exec()
 			g.Assert(err == nil).IsTrue(fmt.Sprintf("Failed to upload stuff: %s", err))
-			//g.Assert(uploaded == 1).IsTrue(fmt.Sprintf("Should have uploaded 1 file instead of %d files", uploaded))
+			// g.Assert(uploaded == 1).IsTrue(fmt.Sprintf("Should have uploaded 1 file instead of %d files", uploaded))
 		})
 
 		g.It("Should upload glob of files", func() {
@@ -46,7 +46,7 @@ func TestPlugin(t *testing.T) {
 
 			err := plugin.Exec()
 			g.Assert(err == nil).IsTrue(fmt.Sprintf("Failed to upload stuff: %s", err))
-			//g.Assert(uploaded == 3).IsTrue(fmt.Sprintf("Should have uploaded 3 files instead of %d files", uploaded))
+			// g.Assert(uploaded == 3).IsTrue(fmt.Sprintf("Should have uploaded 3 files instead of %d files", uploaded))
 		})
 	})
 }
