ad54aa8b0a7e583b5bc186a9fa1a1bc003492f81
JordanSussman <jordan.sussman@target.com>
2018-08-28 11:23:23 -0500

    fix existing tests, update cli version, add additonal testing, and misc. syntax updates

--- main.go
+++ main.go
@@ -23,56 +23,56 @@ func main() {
 		//
 
 		cli.BoolTFlag{
-			Name: "flat",
-			Usage: "artifacts are uploaded to the exact target path specified and their hierarchy in the source file system is ignored",
+			Name:   "flat",
+			Usage:  "artifacts are uploaded to the exact target path specified and their hierarchy in the source file system is ignored",
 			EnvVar: "PLUGIN_FLAT",
 		},
 		cli.BoolFlag{
-			Name: "include-dirs",
-			Usage: "source path applies to bottom-chain directories and not only to files. Bottom-chain directories are either empty or do not include other directories that match the source path",
+			Name:   "include-dirs",
+			Usage:  "source path applies to bottom-chain directories and not only to files. Bottom-chain directories are either empty or do not include other directories that match the source path",
 			EnvVar: "PLUGIN_INCLUDE_DIRS",
 		},
 		cli.StringFlag{
-			Name: "path",
-			Usage: "Where to upload artifacts to",
-			EnvVar: "PLUGIN_PATH",
+			Name:     "path",
+			Usage:    "Where to upload artifacts to",
+			EnvVar:   "PLUGIN_PATH",
 			FilePath: ".artifactory_path",
 		},
 		cli.StringFlag{
-			Name: "password",
-			Usage: "Artifactory server password",
+			Name:   "password",
+			Usage:  "Artifactory server password",
 			EnvVar: "ARTIFACTORY_PASSWORD,PLUGIN_PASSWORD",
 		},
 		cli.StringFlag{
-			Name: "apikey",
-			Usage: "Artifactory API Key",
+			Name:   "apikey",
+			Usage:  "Artifactory API Key",
 			EnvVar: "ARTIFACTORY_APIKEY, PLUGIN_APIKEY",
 		},
 		cli.BoolTFlag{
-			Name: "recursive",
-			Usage: "artifacts are also collected from sub-folders of the source directory for upload.",
+			Name:   "recursive",
+			Usage:  "artifacts are also collected from sub-folders of the source directory for upload.",
 			EnvVar: "PLUGIN_RECURSIVE",
 		},
 		cli.BoolFlag{
-			Name: "regexp",
-			Usage: "command will interpret the sources as a regular expression.",
+			Name:   "regexp",
+			Usage:  "command will interpret the sources as a regular expression.",
 			EnvVar: "PLUGIN_REGEXP",
 		},
 		cli.StringSliceFlag{
 			Name: "sources",
 			Usage: "local file system path to artifacts which should be uploaded to " +
-						 "Artifactory. You can specify multiple artifacts by using " +
-						 "wildcards or a regular expression as designated by the --regexp command option.",
+				"Artifactory. You can specify multiple artifacts by using " +
+				"wildcards or a regular expression as designated by the --regexp command option.",
 			EnvVar: "PLUGIN_SOURCES",
 		},
 		cli.StringFlag{
-			Name: "url",
-			Usage: "Artifactory server URL",
+			Name:   "url",
+			Usage:  "Artifactory server URL",
 			EnvVar: "PLUGIN_URL",
 		},
 		cli.StringFlag{
-			Name: "username",
-			Usage: "Artifactory server username",
+			Name:   "username",
+			Usage:  "Artifactory server username",
 			EnvVar: "ARTIFACTORY_USERNAME,PLUGIN_USERNAME",
 		},
 	}
@@ -94,12 +94,12 @@ func run(c *cli.Context) error {
 			Flat:        c.Bool("flat"),
 			IncludeDirs: c.Bool("include-dirs"),
 			Path:        strings.TrimSpace(c.String("path")),
-			ApiKey:      c.String("apikey"),
+			APIKey:      c.String("apikey"),
 			Password:    c.String("password"),
 			Recursive:   c.Bool("recursive"),
 			Regexp:      c.Bool("regexp"),
 			Sources:     c.StringSlice("sources"),
-			Url:         c.String("url"),
+			URL:         c.String("url"),
 			Username:    c.String("username"),
 		},
 	}
--- plugin_test.go
+++ plugin_test.go
@@ -2,22 +2,23 @@ package main
 
 import (
 	"fmt"
-	// "io"
-	"testing"
-	. "github.com/franela/goblin"
-
-	"github.com/jmccann/drone-artifactory/fixtures"
 	"net/http/httptest"
+	"testing"
+
+	. "github.com/franela/goblin"
+	"github.com/jmccann/drone-artifactory/fixtures"
 )
 
 func TestPlugin(t *testing.T) {
 	g := Goblin(t)
+	p := plugin
 	server := httptest.NewServer(fixtures.Handler())
 
 	g.Describe("Exec", func() {
 		g.Before(func() {
+			p = plugin
 			// Use fake server url
-			plugin.Config.Url = server.URL
+			plugin.Config.URL = server.URL
 
 		})
 		g.After(func() {
@@ -25,38 +26,81 @@ func TestPlugin(t *testing.T) {
 		})
 
 		g.It("Should upload files and directories", func() {
-			err := plugin.Exec()
-			uploaded := len(plugin.Config.Sources)
+			err := p.Exec()
+			uploaded := len(p.Config.Sources)
 			g.Assert(err == nil).IsTrue(fmt.Sprintf("Failed to upload stuff: %s", err))
 			g.Assert(uploaded == 2).IsTrue(fmt.Sprintf("Should have uploaded 2 files instead of %d files", uploaded))
 		})
 
 		g.It("Should upload a file", func() {
 			// Set to a single file
-			plugin.Config.Sources = []string{"main.go"}
-
-			err := plugin.Exec()
+			p.Config.Sources = []string{"main.go"}
+			err := p.Exec()
 			g.Assert(err == nil).IsTrue(fmt.Sprintf("Failed to upload stuff: %s", err))
 		})
 
 		g.It("Should upload glob of files", func() {
 			// Set to a single file
-			plugin.Config.Sources = []string{"*.go"}
+			p.Config.Sources = []string{"*.go"}
 
-			err := plugin.Exec()
+			err := p.Exec()
 			g.Assert(err == nil).IsTrue(fmt.Sprintf("Failed to upload stuff: %s", err))
+		})
+	})
+
+	g.Describe("Validate Input", func() {
+		config := p.Config
+
+		g.BeforeEach(func() {
+			config = p.Config
+		})
+
+		g.It("- should validate input", func() {
+			err := validateInput(config)
+			g.Assert(err == nil).IsTrue(fmt.Sprintf("Failed to validate input: %s", err))
+		})
+		g.It("- should fail on sources", func() {
+			config.Sources = []string{}
+			err := validateInput(config)
+			g.Assert(err != nil).IsTrue()
+			g.Assert(err).Equal(fmt.Errorf("No sources provided"))
+		})
+		g.It("- should fail on password/api key", func() {
+			config.Password = ""
+			config.APIKey = ""
+			err := validateInput(config)
+			g.Assert(err != nil).IsTrue()
+			g.Assert(err).Equal(fmt.Errorf("No ApiKey or Password provided"))
+		})
+		g.It("- should fail on path", func() {
+			config.Path = ""
+			err := validateInput(config)
+			g.Assert(err != nil).IsTrue()
+			g.Assert(err).Equal(fmt.Errorf("No path provided"))
+		})
+		g.It("- should fail on url", func() {
+			config.URL = ""
+			err := validateInput(config)
+			g.Assert(err != nil).IsTrue()
+			g.Assert(err).Equal(fmt.Errorf("No url provided"))
+		})
+		g.It("- should fail on username", func() {
+			config.Username = ""
+			err := validateInput(config)
+			g.Assert(err != nil).IsTrue()
+			g.Assert(err).Equal(fmt.Errorf("No username provided"))
 		})
 	})
 }
 
 var (
 	c = Config{
-		DryRun: true,
-		Path: "thekey/with/path",
+		DryRun:   true,
+		Path:     "thekey/with/path",
 		Password: "supersecret",
-		ApiKey: "apikeyofartifactory",
-		Sources: []string{"main.go", "fixtures/*"},
-		Url: "http://company.com",
+		APIKey:   "apikeyofartifactory",
+		Sources:  []string{"main.go", "fixtures/*"},
+		URL:      "http://company.com",
 		Username: "johndoe",
 	}
 	plugin = Plugin{
